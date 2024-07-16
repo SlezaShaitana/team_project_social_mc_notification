88a8c395fa6eb95a6082f7fd2d3b285ecb9fef6d
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2024-07-16 22:25:32 +0300

    Add mc-notification project files to repository

---
+++ src/main/java/com/social/mcnotification/McNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.social.mcnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/social/mcnotification/McNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.social.mcnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
