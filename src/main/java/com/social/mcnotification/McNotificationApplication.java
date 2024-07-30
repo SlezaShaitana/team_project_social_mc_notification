d20afd4b9b1e96e641a9cb6a9a5f61bb6baa3648
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2024-07-30 21:09:24 +0300

    Added corrections to KafkaMessageService

--- src/main/java/com/social/mcnotification/McNotificationApplication.java
+++ src/main/java/com/social/mcnotification/McNotificationApplication.java
@@ -2,8 +2,10 @@ package com.social.mcnotification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class McNotificationApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/social/mcnotification/kafka/service/KafkaMessageService.java
+++ src/main/java/com/social/mcnotification/kafka/service/KafkaMessageService.java
@@ -3,6 +3,7 @@ package com.social.mcnotification.kafka.service;
 import com.social.mcnotification.client.FriendClient;
 import com.social.mcnotification.dto.NotificationDto;
 import com.social.mcnotification.dto.NotificationSettingDto;
+import com.social.mcnotification.enums.MicroServiceName;
 import com.social.mcnotification.repository.NotificationRepository;
 import com.social.mcnotification.services.helper.Mapper;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -23,19 +25,28 @@ public class KafkaMessageService {
 
     private Mapper mapper;
     private final NotificationRepository notificationRepository;
-
-    private FriendClient friendClient;
-
+    private final FriendClient friendClient;
     private final List<NotificationDto> messages = new ArrayList<>();
 
     public void savingToNotificationRepository(NotificationDto notificationDto) {
+        MicroServiceName microServiceName = notificationDto.getServiceName();
         //Смотреть откуда пришло
 
         //Пример: пришла из постов
         //принимаешь это сообщение
         //смотришь друзей этого пользователя --> friends
         //сохраняешь в БД столько уведомлений, сколько у пользователя друзей, меняя толкьо receiverId
-        notificationRepository.save(mapper.mapToNotificationEntity(notificationDto));
+
+
+        ResponseEntity<List<UUID>> response = friendClient.getFriendsIdListByUserId(notificationDto.getAuthorId());
+        List<UUID> listFriendsId = response.getBody();
+
+        if (listFriendsId != null) {
+            for (UUID uuid : listFriendsId) {
+                notificationDto.setReceiverId(uuid);
+                notificationRepository.save(mapper.mapToNotificationEntity(notificationDto));
+            }
+        }
     }
 
     public void addToList(NotificationDto notificationDto) {
