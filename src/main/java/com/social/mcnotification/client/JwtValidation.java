914364747cbe65e8012b3df536784eab62f6f3ba
Daniil Tishin <danatiska5@gmail.com>
2024-08-22 16:11:04 +0300

    update security settings

--- src/main/java/com/social/mcnotification/client/JwtValidation.java
+++ src/main/java/com/social/mcnotification/client/JwtValidation.java
@@ -1,13 +1,10 @@
 package com.social.mcnotification.client;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(value = "JwtValidation", url = "http://79.174.80.200:8086/api/v1/auth")
-public interface AuthClient {
+public interface JwtValidation {
     @GetMapping("/check-validation")
     Boolean validateToken(@RequestParam("token") String token);
 }
--- src/main/java/com/social/mcnotification/security/jwt/JwtTokenFilter.java
+++ src/main/java/com/social/mcnotification/security/jwt/JwtTokenFilter.java
@@ -1,66 +1,35 @@
 package com.social.mcnotification.security.jwt;
 
-import com.social.mcnotification.client.AuthClient;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import com.social.mcnotification.client.JwtValidation;
+import io.jsonwebtoken.MalformedJwtException;
 import lombok.Getter;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 import java.io.IOException;
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 import java.util.stream.Collectors;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
 
+@Component
 @RequiredArgsConstructor
 @Slf4j
-@Component
 public class JwtTokenFilter extends OncePerRequestFilter {
+
     private final JwtUtils jwtUtils;
-    private final AuthClient authClient;
+    private final JwtValidation jwtValidation;
+
     @Getter
     private User user;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        try {
-            String token = getToken(request);
-            if (token != null && authClient.validateToken(token)) {
-
-                UUID id = UUID.fromString(jwtUtils.getId(token));
-
-                String email = jwtUtils.getEmail(token);
-                List<String> roles = jwtUtils.getRoles(token);
-                user = new User(id, token, email, roles);
-
-                Collection<? extends GrantedAuthority> authorities = roles.stream()
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
-
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        email, null, authorities
-                );
-
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-
-                SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        } catch (Exception e) {
-            log.info("JWT token validation failed");
-        }
-        filterChain.doFilter(request, response);
-    }
 
     private String getToken(HttpServletRequest request) {
         String headerAuth = request.getHeader("Authorization");
@@ -68,6 +37,43 @@ public class JwtTokenFilter extends OncePerRequestFilter {
         if (StringUtils.hasText(headerAuth) && headerAuth.startsWith("Bearer ")) {
             return headerAuth.substring(7);
         }
-        return null;
+        log.error("Request is empty or damaged");
+        throw new IllegalArgumentException("Authorization header is missing or malformed");
+    }
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        try {
+            String token = getToken(request);
+            log.info("Token: '{}'", token);
+
+            if (jwtValidation.validateToken(token)) {
+                UUID id = UUID.fromString(jwtUtils.getId(token));
+                String email = jwtUtils.getEmail(token);
+                List<String> roles = jwtUtils.getRoles(token);
+                user = new User(id, token, email, roles);
+                Collection<? extends GrantedAuthority> authorities = roles.stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList());
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        email, null, authorities);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            } else {
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+        } catch (MalformedJwtException e) {
+            log.error("Invalid JWT token format: {}", e.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        } catch (Exception e) {
+            log.error("JWT token validation failed: {}", e.getMessage());
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            return;
+        }
+        filterChain.doFilter(request, response);
     }
 }
