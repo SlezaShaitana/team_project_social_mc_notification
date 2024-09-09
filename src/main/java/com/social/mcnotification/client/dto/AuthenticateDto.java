86a1e7e1351fea8afd808ce63d110d74cb7ffcfc
@aleksandra_khamenok <sleza.shaitana.1@gmail.com>
2024-09-09 22:27:59 +0300

    update

--- src/main/java/com/social/mcnotification/client/dto/AuthenticateDto.java
+++ src/main/java/com/social/mcnotification/client/dto/AuthenticateDto.java
@@ -1,11 +1,13 @@
 package com.social.mcnotification.client.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Setter;
 
 @AllArgsConstructor
 @Data
+@Builder
 public class AuthenticateDto {
     private String email;
     private String password;
